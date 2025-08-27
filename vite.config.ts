286cf0259a7a4616a44afe22f708614a4318e8d3
Sharmila127 <sharmila6201@gmail.com>
2025-08-27 14:11:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/partner/',   // (CloudFront path prefix)
-  build: {
+	plugins: [react()],
+	build: {
     outDir: 'dist',
     assetsDir: 'assets',
   }
-})
+});
