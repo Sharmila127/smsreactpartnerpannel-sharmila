bc177cb75bfb4c1c2114bc768d6c6f939924ffff
Sharmila127 <sharmila6201@gmail.com>
2025-08-28 10:10:22 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	build: {
+  base: '/partner/',   // 👈 Partner app path
+  plugins: [react()],
+  build: {
     outDir: 'dist',
     assetsDir: 'assets',
-  }
+  },
 });
